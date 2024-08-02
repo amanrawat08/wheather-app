4634016a1972a39c30e1711c696dec0d931d0704
amanrawat08 <ar7541147@gmail.com>
2024-08-02 10:27:06 +0530

    api

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,37 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <>Wheather app</>;
+  async function wheather() {
+    try {
+      const response = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=uttarakhand&appid=a9eb9818eb9418723362b80ec71e87a9"
+      );
+      const data = await response.json();
+      console.log(data);
+      console.log("Name : " + data.name);
+      console.log("Cloud : " + data.clouds.all);
+      console.log("temp : " + data.main.temp);
+      console.log("Clouds : " + data.clouds.all);
+      console.log("Humidity : " + data.main.humidity);
+      console.log("Country Code : " + data.sys.country);
+      console.log("Wind Speed : " + data.wind.speed);
+      console.log("Weather Type :" + data.weather[0].main);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          wheather();
+        }}
+      >
+        Click
+      </button>
+    </>
+  );
 }
 
 export default App;
